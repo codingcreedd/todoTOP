52834896e4a96ecfd113eb1f09804e687b0bfe5d
codingcreedd <moubayedmarwancoding@gmail.com>
2024-02-11 15:35:26 +0200

    Add basic event listeners

--- src/DOMModule.js
+++ src/DOMModule.js
@@ -1,9 +1,18 @@
 import toDoModule from './index.js';
+import domMethods from './domMethods.js';
+import {renderTaskForm} from "../src/renderingDOM";
 
 const domModule = (() => {
     const addTaskButton = document.querySelector('.add-task-btn');
-    addTaskButton.addEventListener('click', () => {
-        console.log('dfdffd');
-        document.querySelector('.task-form-container').style.display = 'flex';
-    });
+    addTaskButton.addEventListener('click', renderTaskForm);
+
+    const submitTaskButton = document.querySelector('.add-input-btn');
+    submitTaskButton.addEventListener('click', domMethods.addTaskToList);
+
+    const cancelTaskButton = document.querySelector('.cancel-input-btn');
+    cancelTaskButton.addEventListener('click', domMethods.cancelTaskFromList);
+
+    const importantNavButton = document.querySelector('.important-nav-btn');
+    importantNavButton.addEventListener('click', renderInImportant);
+
 })();
