bb6085dc53339b07b29b06c583a5d70691fde8a2
codingcreedd <moubayedmarwancoding@gmail.com>
2024-02-11 10:29:03 +0200

    Add necessary files like JSON files & Webpack Configuration

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: ['./src/index.js',],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'To Do',
+            filename: 'index.html',
+            template: 'src/template.html',
+        }),
+    ],
+};
