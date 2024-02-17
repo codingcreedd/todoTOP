896f5521c8426bbc1df6181bb072f670f098a82b
codingcreedd <moubayedmarwancoding@gmail.com>
2024-02-17 21:21:18 +0200

    Change validation.js using prettier

--- src/validation.js
+++ src/validation.js
@@ -1,36 +1,32 @@
-const errorDiv = document.querySelector('.error-div');
-errorDiv.classList.add('hidden');
-
+const errorDiv = document.querySelector(".error-div");
+errorDiv.classList.add("hidden");
 
 export const checkTaskTitleValidation = () => {
-    const taskTitleInput = document.querySelector('.task-title-input');
+  const taskTitleInput = document.querySelector(".task-title-input");
 
-    if(taskTitleInput.validity.tooShort || taskTitleInput.value === ''){
-        errorDiv.classList.toggle('hidden');
-        errorDiv.textContent = "Your title or description is too short!";
-        return false;
-    }
-    else
-    {
-        errorDiv.textContent = "";
-        if(!errorDiv.classList.contains('hidden'))
-            errorDiv.classList.add('hidden');
+  if (taskTitleInput.validity.tooShort || taskTitleInput.value === "") {
+    errorDiv.classList.toggle("hidden");
+    errorDiv.textContent = "Your title or description is too short!";
+    return false;
+  } else {
+    errorDiv.textContent = "";
+    if (!errorDiv.classList.contains("hidden"))
+      errorDiv.classList.add("hidden");
 
-        return true;
-    }
-
+    return true;
+  }
 };
 
 export const checkTaskDescriptionValidation = () => {
-    const taskDescriptionInput = document.querySelector('.task-description-input');
+  const taskDescriptionInput = document.querySelector(
+    ".task-description-input",
+  );
 
-    if(taskDescriptionInput.validity.tooShort){
-        errorDiv.classList.toggle('hidden');
-        errorDiv.textContent = "Your title or description is too short!";
-        return false;
-    }
-    else
-    {
-        return true;
-    }
-}
+  if (taskDescriptionInput.validity.tooShort) {
+    errorDiv.classList.toggle("hidden");
+    errorDiv.textContent = "Your title or description is too short!";
+    return false;
+  } else {
+    return true;
+  }
+};
